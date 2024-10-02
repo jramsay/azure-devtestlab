be83b9a0dadc1ccd887af26ebb177eed5080d849
Jason Ramsay <jasonra@microsoft.com>
2024-10-02 12:01:38 -0700

    add test scripts

---
+++ Scripts/checkout_repo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Variables
+REPO_URL=${1:-"https://github.com/psf/requests.git"}
+CLONE_DIR=${2:-""}
+BRANCH_NAME=${3:-"main"}
+
+# Clone the repository
+git clone $REPO_URL $CLONE_DIR
+
+# Navigate into the repository directory
+cd $CLONE_DIR
+
+# Checkout a specific branch (optional)
+git checkout $BRANCH_NAME
+
+echo "Repository cloned and checked out to branch $BRANCH_NAME"
---
+++ Scripts/clone_repo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+# Clone the repository
+git clone https://github.com/psf/requests.git
