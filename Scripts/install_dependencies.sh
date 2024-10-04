5949338a628c1c2ea12b6e0a1d80bccbd27c29c0
Jason Ramsay <jasonra@microsoft.com>
2024-10-04 11:58:51 -0700

    install python updates

--- Scripts/install_dependencies.sh
+++ Scripts/install_dependencies.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-cp *.sh /home/jasonra/
+export HOME=${1:-"/home/devtestlab"}
+export PYTHON_VERSION=${2:-"3.12"}
+
+mkdir -p $HOME
+cp *.sh $HOME
+cd $HOME
+sh $HOME/install_python.sh
--- Scripts/install_python.sh
+++ Scripts/install_python.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# Variables
-export HOME=$(pwd)
-PYTHON_VERSION="3.12"
-
 # Set up Python
-
 echo "Setting up Python $PYTHON_VERSION"
 
 # Install pyenv
 curl https://pyenv.run | bash
 
 # Add pyenv to PATH and initialize
-
 export PATH="$HOME/.pyenv/bin:$PATH"
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
+
 eval "$(pyenv init --path)"
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+
 eval "$(pyenv init -)"
+
 eval "$(pyenv virtualenv-init -)"
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 
 # Install the specified Python version
 pyenv install $PYTHON_VERSION
