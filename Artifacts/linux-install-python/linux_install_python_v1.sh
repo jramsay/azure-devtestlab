e7c93374b7f98ea730573be310b727523d055c12
Jason Ramsay <jasonra@microsoft.com>
2024-10-04 08:19:46 -0700

    python artifact

---
+++ Artifacts/linux-install-python/linux_install_python_v1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Variables
+export HOME=$(pwd)
+PYTHON_VERSION="3.12"
+
+# Set up Python
+
+echo "Setting up Python $PYTHON_VERSION"
+
+# Install pyenv
+curl https://pyenv.run | bash
+
+# Add pyenv to PATH and initialize
+
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+# Install the specified Python version
+pyenv install $PYTHON_VERSION
+pyenv global $PYTHON_VERSION
+
+# Upgrade pip and install pip cache
+pip install --upgrade pip
+pip install pip-cache
+
+echo "Python $PYTHON_VERSION setup complete with pip cache enabled."
