834df83332b4c80e05648fae5431001516518dcd
Jason Ramsay <jasonra@microsoft.com>
2024-10-30 15:42:36 -0700

    add additional scripts

---
+++ Scripts/host_tunnel.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Variables
+PORT_NUMBER=${1:-5000}
+
+output=$(devtunnel login --mi-client-id 8286efbb-1b94-4821-8876-b87156372c08)
+echo "$output"
+
+output=$(devtunnel create 2>&1)
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+    echo "Create tunnel failed: $output"
+else
+    echo "Create tunnel succeeded"
+    tunnel_id=$(echo "$output" | grep "Tunnel ID" | awk -F ': ' '{print $2}')
+    echo "Tunnel ID: $tunnel_id"
+
+    output=$(devtunnel port create -p $PORT_NUMBER 2>&1)
+    exit_status=$?
+    if [ $exit_status -ne 0 ]; then
+        echo "Create port failed: $output"
+    else
+        echo "Create port succeeded"
+        port_number=$(echo "$output" | grep "Port Number" | awk -F ': ' '{print $2}')
+        echo "Port Number: $port_number"
+    fi
+fi
+
+
--- Scripts/install_dependencies.sh
+++ Scripts/install_dependencies.sh
@@ -7,3 +7,16 @@ cp *.sh $HOME
 cd $HOME
 ln -s /usr/bin/python3 /usr/bin/python
 python3 -m venv virtual_env
+
+pip3 install Flask
+
+python3 app.py &
+pid=$!
+
+echo "Python app launched with PID: $pid"
+
+sudo apt-get update
+sudo apt-get install curl
+curl -sL https://aka.ms/DevTunnelCliInstall | bash
+source ~/.bashrc
+
