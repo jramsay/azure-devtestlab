a9021386bba9fc269621b3fb85b9e4782ab33d28
Jason Ramsay <jasonra@microsoft.com>
2025-02-05 15:29:45 -0800

    working

--- Scripts/auto-test/setup.ps1
+++ Scripts/auto-test/setup.ps1
@@ -11,6 +11,19 @@ if (-Not (Test-Path -Path $destinationDirectory)) {
 
 Copy-Item -Path "$sourceDirectory\*" -Destination $destinationDirectory -Recurse
 
-Write-Output "Set up server..."
-$setupServer = "$setupPath\setup-auto-test-server.ps1"
-Invoke-Expression -Command $setupServer
+Set-Location -Path $setupPath
+
+Write-Output "Installing python..."
+$installPython = "$setupPath\install-python.ps1"
+Invoke-Expression -Command $installPython
+Write-Output "Python installation completed."
+
+Write-Output "Installing conda..."
+$condaScript = "$setupPath\install-conda.ps1"
+$condaProcess = Start-Process powershell -ArgumentList "-File `"$condaScript`"" -PassThru
+$condaProcess.WaitForExit()
+Write-Output "Conda installation completed."
+
+Write-Output "Installing dev tunnel..."
+$installDevTunnel = "$setupPath\install-devtunnel.ps1"
+Invoke-Expression -Command $installDevTunnel
