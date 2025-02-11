6b1a2dc29a2caf70709e2bfd1a03ee3b667d6bc2
Jason Ramsay <jasonra@microsoft.com>
2025-02-10 17:16:17 -0800

    working

--- Scripts/auto-test/launch-server-and-tunnel.ps1
+++ Scripts/auto-test/launch-server-and-tunnel.ps1
@@ -11,7 +11,7 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
 
 Write-Output "Launching server..."
 $launchServer = "$setupPath\launch-server.ps1"
-Start-Process powershell -ArgumentList "-NoExit -File `"$launchServer`"" -RedirectStandardOutput "server-log.txt"
+Start-Process powershell -ArgumentList "-NoExit -File `"$launchServer`""
 
 Write-Output "Starting dev tunnel..."
 $launchDevTunnel = "$setupPath\launch-devtunnel.ps1"
--- Scripts/auto-test/launch-server.ps1
+++ Scripts/auto-test/launch-server.ps1
@@ -11,4 +11,4 @@ py -m pip install -r requirements.txt
 # Temp fix: install specific version of autogen
 py -m pip install autogen-agentchat==0.2.37
 
-py -m flask --app server run
+py -m flask --app server run > server-log.txt 2>&1
