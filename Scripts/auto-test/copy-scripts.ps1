5f12e692898181f755f7e4360926ca1ad3539391
Jason Ramsay <jasonra@microsoft.com>
2025-02-12 16:39:07 -0800

    working

---
+++ Scripts/auto-test/copy-scripts.ps1
@@ -0,0 +1,12 @@
+param (
+    [string]$setupPath = "C:\setup"
+)
+
+$sourceDirectory = Get-Location
+$destinationDirectory = $setupPath
+
+if (-Not (Test-Path -Path $destinationDirectory)) {
+    New-Item -ItemType Directory -Path $destinationDirectory
+}
+
+Copy-Item -Path "$sourceDirectory\*" -Destination $destinationDirectory -Recurse
