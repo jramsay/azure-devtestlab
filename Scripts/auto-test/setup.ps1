7e3ed1791f9ec6bf902fec10daf66dabd002d2ec
Jason Ramsay <jasonra@microsoft.com>
2025-02-13 16:03:33 -0800

    working

--- Scripts/auto-test/setup.ps1
+++ Scripts/auto-test/setup.ps1
@@ -1,10 +1,16 @@
 param (
+    [string]$username,
+    [string]$password,
     [string]$setupPath = "C:\setup"
 )
 
-Set-Location -Path $setupPath
 
-Unregister-ScheduledTask -TaskName "RunScriptAfterLogon" -Confirm:$false
+$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+Set-ItemProperty -Path $registryPath -Name "DefaultUserName" -Value $username
+Set-ItemProperty -Path $registryPath -Name "DefaultPassword" -Value $password
+Set-ItemProperty -Path $registryPath -Name "AutoAdminLogon" -Value "1"
+
+Set-Location -Path $setupPath
 
 Write-Output "Installing python..."
 $installPython = "$setupPath\install-python.ps1"
@@ -19,6 +25,8 @@ Write-Output "UV installation completed."
 Write-Output "Installing dev tunnel..."
 Invoke-WebRequest -Uri https://aka.ms/TunnelsCliDownload/win-x64 -OutFile devtunnel.exe
 
-Write-Output "Launching server and tunnel..."
+# Create a scheduled task to launch the server & tunnel at startup after autologon
 $launchServer = "$setupPath\launch-server-and-tunnel.ps1"
-Invoke-Expression -Command $launchServer
+schtasks /create /tn "RunScriptAtStartup" /tr "powershell.exe -File $launchServer" /sc onstart /rl highest /f
+
+Restart-Computer -Force
