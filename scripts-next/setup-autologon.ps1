89ee067ed6ca885be47fb7aa34dca548c5d0d52e
Jason Ramsay <jasonra@microsoft.com>
2025-04-07 22:13:19 -0700

    working

--- scripts-next/setup-autologon.ps1
+++ scripts-next/setup-autologon.ps1
@@ -32,4 +32,4 @@ if (-not (Test-Path $privacyPoliciesRegistryPath)) {
 New-ItemProperty -Path $privacyPoliciesRegistryPath -Name $disablePrivacy -Value 1 -PropertyType DWord -Force
 
 # Disable Windows Backup dialog
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsBackup" -Name "DisableMonitoring" -Value 1 -PropertyType DWORD
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsBackup" -Name "DisableMonitoring" -Value 1
